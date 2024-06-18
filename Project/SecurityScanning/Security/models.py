9ac07ae93ca1c50068ba982f92c420f2c1559557
Guru Pruthvi J M <gurupruthvi61@gmail.com>
2024-06-19 04:17:07 +0530

    f

---
+++ Project/SecurityScanning/Security/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Query(models.Model):
+    useremail = models.CharField(max_length=50)
+    username = models.CharField(max_length=100)
+    userquery = models.TextField(default="NULL")
+
+    class Meta:
+        app_label = 'Security'
